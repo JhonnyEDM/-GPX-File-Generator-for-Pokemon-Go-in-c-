dbaa5f1885541afb033783d170d674421d3141bc
Jhonny Ortiz <Josmeledm@gmail.com>
2019-04-03 08:39:02 +0800

    Generador de GPX
    Se Genera el archivo GPX en la raiz de la aplicacion, faltan mejoras

---
+++ ProjectoGPX/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ProjectoGPX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Coordenada = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 277);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Coordenada
+            // 
+            this.Coordenada.AutoSize = true;
+            this.Coordenada.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Coordenada.Location = new System.Drawing.Point(67, 9);
+            this.Coordenada.Name = "Coordenada";
+            this.Coordenada.Size = new System.Drawing.Size(169, 25);
+            this.Coordenada.TabIndex = 2;
+            this.Coordenada.Text = "COORDENADAS";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(53, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generar GPX";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 407);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Coordenada);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Coordenada;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ProjectoGPX/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ProjectoGPX
+{
+    public partial class Form1 : Form
+    {
+        string linea;
+        string aquiestartodas;
+        List<string> todas = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string Cabecera = "<?xml version = \"1.0\" encoding=\"UTF - 8\"?> <gpx>";
+                string Pie = "</gpx>";
+                string Latinicio = "<wpt lat=\"";
+                string Loninicio = "\"lon=\"";
+                string Lonfinal= "\"></wpt>";
+
+                var hola = textBox1.Text;
+                string[] corrdenadas = hola.Split('\n');
+
+                foreach (var item in corrdenadas)
+                {
+                    string[] final = item.Split(',');
+                    string longitud = final[1].Trim();
+                    todas.Add(linea = Latinicio + final[0] + Loninicio + longitud + Lonfinal);
+                }
+
+                foreach (var una in todas)
+                {
+                    aquiestartodas = aquiestartodas + una;
+                }
+                var elfinai = Cabecera + aquiestartodas + Pie;
+
+                File.WriteAllText(Application.StartupPath +"\\Coordenadas.gpx", elfinai);
+                MessageBox.Show("SE GENERO LAS COORDENADAS CON EXITO");
+                textBox1.Clear();
+                textBox1.Focus();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("TE JODISTE ARIELITO" , "\n " + ex.Message);
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+    }
+}
